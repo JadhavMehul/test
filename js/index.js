24b43ac85d04538fe75892e411000ccf15127450
JadhavMehul <jadhavmehuljadhav.mj@gmail.com>
2021-01-09 23:25:27 +0530

    testing day1

---
+++ js/index.js
@@ -0,0 +1,120 @@
+function First() {
+    checkUser = "aroshi";
+    checkPass = "aro1569"
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+            location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Second() {
+    checkUser = "arohi";
+    checkPass = "aro1679";
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+            location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Third() {
+    checkUser = "falguni";
+    checkPass = "fal1789";
+    checkUser1 = "ayush";
+    checkPass1 = "ayu3789";
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+        location.href = "";
+    } else if(userId === checkUser1 && userPass === checkPass1) {
+        location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+
+function Fifth() {
+    checkUser = "eshwari";
+    checkPass = "esh1589"
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+            location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Sixth() {
+    checkUser = "samarth";
+    checkPass = "sam1679";
+    checkUser1 = "prince";
+    checkPass1 = "pri1479";
+    checkUser2 = "monisha";
+    checkPass2 = "mon1257";
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+            location.href = "";
+    } else if(userId === checkUser1 && userPass === checkPass1) {
+        location.href = "";
+    } else if(userId === checkUser2 && userPass === checkPass2) {
+        location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Seventh() {
+    checkUser = "gauri";
+    checkPass = "gau2578";
+    checkUser1 = "aditya";
+    checkPass1 = "adi3467";
+    checkUser2 = "furkan";
+    checkPass2 = "fur4789";
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+        location.href = "";
+    } else if(userId === checkUser1 && userPass === checkPass1) {
+        location.href = "";
+    } else if(userId === checkUser2 && userPass === checkPass2) {
+        location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Eighth() {
+    checkUser = "tanishq";
+    checkPass = "tani5689"
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+            location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
+
+function Ninth() {
+    checkUser = "pratik";
+    checkPass = "pra1239";
+    checkUser1 = "abhishek";
+    checkPass1 = "abhi1579";
+    userId = document.getElementById("exampleInputEmail1").value;
+    userPass = document.getElementById("exampleInputPassword1").value;
+    if (userId === checkUser && userPass === checkPass) {
+        location.href = "";
+    } else if(userId === checkUser1 && userPass === checkPass1) {
+        location.href = "";
+    } else {
+        document.getElementById("validate").style.display = "block";
+    }
+}
